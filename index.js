364bfe0541daca16f92c6a152891335d270ed488
MATEUSDOSSANTO04 <136021585+MATEUSDOSSANTO04@users.noreply.github.com>
2023-08-09 21:31:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import EntradaDados from 'readline-sync';
+import * as EntradaDados from 'readline-sync';
 
 console.log("Nome completo \n");
 
-var nome = EntradaDados.question("digite seu nome: ");
-var sobrenome = EntradaDados.question("Digite seu Sobrenome: \n");
+var nome = EntradaDados.question("Digite seu nome: ");
+var sobrenome = EntradaDados.question("Digite seu sobrenome: \n");
 
-console.log("Nome completo: " + nome + " " + sobrenome)
+console.log("Nome completo: " + nome + " " + sobrenome);
+
+
